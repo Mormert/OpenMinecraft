0796ea1302f70a74dd0fec64b320da3ff80556ce
Johan Lind <Github@mormert.com>
2022-05-01 23:44:38 +0200

    Added brightness sensor data transmission

--- EnvSensor/main.cpp
+++ EnvSensor/main.cpp
@@ -4,9 +4,14 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <signal.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
+#include <time.h>
 #include <fcntl.h>
+
+#include "DEV_Config.h"
+#include "TSL2591.h"
 
 #include <thread>
 #include <atomic>
@@ -16,21 +21,18 @@ using json = nlohmann::json;
 
 std::atomic<float> g_humidity, g_brightness, g_temperature;
 
-void read_data_thread()
-{
+void read_data_thread_humidity_temp() {
     // Create I2C bus
     int file;
     const char *bus = "/dev/i2c-1";
-    if((file = open(bus, O_RDWR)) < 0)
-    {
+    if ((file = open(bus, O_RDWR)) < 0) {
         printf("Failed to open the bus. \n");
         exit(1);
     }
     // Get I2C device, SI7021 I2C address is 0x40(64)
     ioctl(file, I2C_SLAVE, 0x40);
 
-    while(true)
-    {
+    while (true) {
         // Send humidity measurement command(0xF5)
         char config[1] = {0xF5};
         write(file, config, 1);
@@ -39,12 +41,9 @@ void read_data_thread()
         // Read 2 bytes of humidity data
         // humidity msb, humidity lsb
         char data[2] = {0};
-        if(read(file, data, 2) != 2)
-        {
+        if (read(file, data, 2) != 2) {
             printf("Error : Input/output Error \n");
-        }
-        else
-        {
+        } else {
             // Convert the data
             float humidity = (((data[0] * 256 + data[1]) * 125.0) / 65536.0) - 6;
             g_humidity = humidity;
@@ -60,12 +59,9 @@ void read_data_thread()
 
         // Read 2 bytes of temperature data
         // temp msb, temp lsb
-        if(read(file, data, 2) != 2)
-        {
+        if (read(file, data, 2) != 2) {
             printf("Error : Input/output Error \n");
-        }
-        else
-        {
+        } else {
             // Convert the data
             float cTemp = (((data[0] * 256 + data[1]) * 175.72) / 65536.0) - 46.85;
             float fTemp = cTemp * 1.8 + 32;
@@ -82,9 +78,45 @@ void read_data_thread()
 
 }
 
+void read_data_thread_brightness() {
+
+    DEV_ModuleInit();
+
+    TSL2591_Init();
+    // TSL2591_SET_InterruptThreshold(0xff00, 0x0010);
+    while (1) {
+
+        int readLux = TSL2591_Read_Lux();
+
+        g_brightness = readLux;
+
+        //printf("Lux = %d\r\n", readLux);
+        TSL2591_SET_LuxInterrupt(50, 200);
+        //printf("Infrared light: %d\r\n", TSL2591_Read_Infrared());
+        //printf("Visible light: %d\r\n", TSL2591_Read_Visible());
+        //printf("Full spectrum (IR + visible) light: %d\r\n\r\n", TSL2591_Read_FullSpectrum());
+
+
+    }
+    // System Exit
+    DEV_ModuleExit();
+}
+
+void ExceptionHandler(int signo) {
+    //System Exit
+    printf("\r\nCTRL + C Exit...\r\n");
+    DEV_ModuleExit();
+
+    exit(0);
+}
+
 int main(int argc, char **argv) {
 
-    std::thread dataReadThread{read_data_thread};
+    // Exception handling: 'Ctrl + C'
+    signal(SIGINT, ExceptionHandler);
+
+    std::thread dataReadThread_humid_temp{read_data_thread_humidity_temp};
+    std::thread dataReadThread_brightness{read_data_thread_brightness};
 
     httplib::Client cli("http://hexablo:314");
 
@@ -93,14 +125,15 @@ int main(int argc, char **argv) {
     while (true) {
 
         json j{
-                {"humidity",        float(g_humidity)},
-                {"temperature",     float(g_temperature)},
-                {"brightness",      float(g_brightness)}
+                {"humidity",    float(g_humidity)},
+                {"temperature", float(g_temperature)},
+                {"brightness",  float(g_brightness)}
         };
 
         const auto data = j.dump();
+        std::cout << data << '\n';
 
-        cli.Post("/env",data, "application/json");
+        cli.Post("/env", data, "application/json");
 
         std::this_thread::sleep_for(200ms);
     }
